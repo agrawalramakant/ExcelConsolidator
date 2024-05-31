09295f4b94da645a2580fb3fe6590246d129ce95
Ramakant Agrawal <r.agrawal@reply.de>
2024-05-31 18:44:44 +0200

    reformatted the code

--- steps1.py
+++ steps1.py
@@ -3,16 +3,15 @@ from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
 
-def remove_new_sheet_if_exists(file_path):
+def remove_new_sheet_if_exists(file_path, sheet_name):
     book = load_workbook(file_path)
-    if 'New Sheet' in book.sheetnames:
-        del book['New Sheet']
-    book.save('data/May24/Summary sale May-24.xlsx')
+    if sheet_name in book.sheetnames:
+        del book[sheet_name]
+    book.save(file_path)
 
 
 def read_and_aggregate(file_path):
     df = pd.read_excel(file_path, header=7)
-
 
     aggregations = {
         'Date': 'first',  # Just take the first date encountered
@@ -32,12 +31,13 @@ def read_and_aggregate(file_path):
     # Group by 'Vch No.' and apply the defined aggregations
     aggregated_df = df.groupby('Vch No.').agg(aggregations).reset_index()
     aggregated_df['Date'] = pd.to_datetime(aggregated_df['Date']).dt.strftime('%d %b %y')
+
     return aggregated_df
 
 
 def write_to_excel(file_path, aggregated_df, sheet_name):
     with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
-        aggregated_df.to_excel(writer, sheet_name=sheet_name, index=False)
+        aggregated_df.to_excel(writer, sheet_name=sheet_name, index=False, startrow=7)
 
 
 def adjust_column_width(file_path, sheet_name):
@@ -62,7 +62,7 @@ def adjust_column_width(file_path, sheet_name):
 if __name__ == '__main__':
     file_path = 'data/May24/Summary sale May-24.xlsx'
     sheet_name = 'Summary'
-    remove_new_sheet_if_exists(file_path)
+    remove_new_sheet_if_exists(file_path, sheet_name)
     df = read_and_aggregate(file_path)
     write_to_excel(file_path, df, sheet_name)
     adjust_column_width(file_path, sheet_name)
